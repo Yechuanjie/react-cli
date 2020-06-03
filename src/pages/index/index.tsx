9fbd7b8e6f23fc0aaa6f4a50f6b0260b7b0e6e13
hwq <1102684711@qq.com>
2020-06-03 16:01:31 +0800

    update

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,23 +1,10 @@
 import React, { useState, useEffect } from 'react'
 
-import { TabBar } from 'antd-mobile'
-
 import Loading from '@/components/loading'
 
 import logo from '@/assets/logo192.png'
 
 import './index.scss'
-
-const tabList = [
-  {
-    icon: '',
-    title: '首页'
-  },
-  {
-    icon: '',
-    title: '关于'
-  }
-]
 
 function Index() {
   const [showLoading, setshowLoading] = useState(true)
@@ -25,9 +12,7 @@ function Index() {
     const timer = setTimeout(() => {
       setshowLoading(false)
     }, 2000)
-    return () => {
-      clearTimeout(timer)
-    }
+    return () => clearTimeout(timer)
   }, [])
 
   return (
@@ -35,11 +20,6 @@ function Index() {
       <Loading show={showLoading} txt="加载中..."></Loading>
       <img src={logo} alt="" />
       <div>首页...</div>
-      <TabBar>
-        {tabList.map((item, index) => {
-          // return <TabBar.Item icon={{item.icon}} title={item.title}></TabBar.Item>
-        })}
-      </TabBar>
     </div>
   )
 }
