55ec492a89c76f2bd415fea5603b2197277d2519
hwq <1102684711@qq.com>
2020-06-09 17:41:18 +0800

    update

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import request from './request'
 
-export const getList = (params: { type: number }) => request('/api/getInfo', 'GET', {}, true)
+export const getList = (params: { type: number }) => request('/api/getInfo', 'GET', { ...params }, true)
 
 export const updateInfo = (params: { name: string; phone: string; password: string }) =>
   request('/api/updateInfo', 'POST', { ...params }, true)
