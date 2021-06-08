723c7938180d448d0c54dc37216f76caae3b81a7
yechuanjie <1102684711@qq.com>
2021-06-08 10:46:18 +0000

    Update

---
+++ precache-manifest.3e6986d4e36b1a52be39a69661d17dfc.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a840d6fd0d3c885ab7e5a7b82385385d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "67de8a6d559780a37ecb",
+    "url": "./static/css/5.e1073d80.chunk.css"
+  },
+  {
+    "revision": "6d44e674f5bbd5da41f2",
+    "url": "./static/css/6.07b8716a.chunk.css"
+  },
+  {
+    "revision": "5fd6a4a91d531579786d",
+    "url": "./static/css/About.dd9c0c5f.chunk.css"
+  },
+  {
+    "revision": "cad5204f1871feef04b3",
+    "url": "./static/css/Detail.645242c8.chunk.css"
+  },
+  {
+    "revision": "79dadc612f473eab073d",
+    "url": "./static/css/Index.bf2d9452.chunk.css"
+  },
+  {
+    "revision": "7bfac4c2a88a357f6ac4",
+    "url": "./static/css/main.ebc268a8.chunk.css"
+  },
+  {
+    "revision": "67de8a6d559780a37ecb",
+    "url": "./static/js/5.de9ab6fe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/5.de9ab6fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d44e674f5bbd5da41f2",
+    "url": "./static/js/6.00e374c4.chunk.js"
+  },
+  {
+    "revision": "5fd6a4a91d531579786d",
+    "url": "./static/js/About.cd0f48fd.chunk.js"
+  },
+  {
+    "revision": "cad5204f1871feef04b3",
+    "url": "./static/js/Detail.004b0286.chunk.js"
+  },
+  {
+    "revision": "79dadc612f473eab073d",
+    "url": "./static/js/Index.2ab63068.chunk.js"
+  },
+  {
+    "revision": "7bfac4c2a88a357f6ac4",
+    "url": "./static/js/main.0ec694fb.chunk.js"
+  },
+  {
+    "revision": "39e7a7d9036531faac67",
+    "url": "./static/js/runtime-main.e4fda7f3.js"
+  },
+  {
+    "revision": "15f8750ab7195268ae90cb545e777dbf",
+    "url": "./static/media/preload-bg@2x.15f8750a.png"
+  },
+  {
+    "revision": "90da4dd69ec27b13b86d3df730263530",
+    "url": "./static/media/preload-inner@2x.90da4dd6.png"
+  },
+  {
+    "revision": "e9b1ca72c09012e32a9aa86b38bc820a",
+    "url": "./static/media/qrcode.e9b1ca72.png"
+  }
+]);
